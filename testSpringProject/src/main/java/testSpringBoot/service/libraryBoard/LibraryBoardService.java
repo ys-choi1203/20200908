c1c6262fc6263d8ecfcfb7a4a725ed1b690773af
ho041 <ho041@LAPTOP-4MOVCS4H>
2020-09-08 14:55:37 +0900

    commit2

--- testSpringProject/src/main/java/testSpringBoot/service/libraryBoard/LibraryBoardService.java
+++ testSpringProject/src/main/java/testSpringBoot/service/libraryBoard/LibraryBoardService.java
@@ -1,16 +1,37 @@
 package testSpringBoot.service.libraryBoard;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import testSpringBoot.command.AuthInfo;
 import testSpringBoot.command.LibraryBoardCommand;
+import testSpringBoot.domain.LibraryBoardDTO;
 
+@Component
 @Service
 public class LibraryBoardService {
 
 	public void writePro(LibraryBoardCommand libraryBoardCommand, HttpServletRequest request) {
-		// TODO Auto-generated method stub
+		 LibraryBoardDTO libraryBoardDTO = new LibraryBoardDTO();
+		 
+		 libraryBoardDTO.setBoardContent(libraryBoardCommand.getBoardContent());
+		 libraryBoardDTO.setBoardName(libraryBoardCommand.getBoardName());
+		 libraryBoardDTO.setBoardPass(libraryBoardCommand.getBoardPass());
+		 libraryBoardDTO.setBoardSubject(libraryBoardCommand.getBoardSubject());
+		 HttpSession session = request.getSession();
+		 AuthInfo authInfo = (AuthInfo) session.getAttribute("authInfo");
+		 libraryBoardDTO.setUserId(authInfo.getId());
+		 libraryBoardDTO.setIpAddr(request.getRemoteAddr());
+		 
+		 // 파일 정보를 입력하기 위한 변수
+		 String originalTotal = "";
+		 String stroeTotal = "";
+		 String fileSizeTotal = "";
+		 String location = "";
+		 String PATH = "/static/lib_Board/upload";
 		
 	}
 
